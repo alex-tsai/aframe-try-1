ed438b45065dbe955083f25c001965954a83aa08
Alex Tsai <al3x.tsai@gmail.com>
2015-12-30 00:49:09 +0800

    rotating cubes

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	for(var x = 0; x < 5; x++)
+	{
+		var cube = $("<a-cube color='#92A5A5' position='" + (x*3-6) + " " +Math.random()*3+ " -5'></a-cube>");
+		$('#scene').append(cube);
+		cube.append("<a-animation attribute='rotation' to='0 " + Math.random()*360 + " " + Math.random()*360 + "' dur='5000' direction='alternate' fill='forwards' repeat='indefinite' ease='linear'></a-animation>");
+		
+	}
+});
