bb266d8da6bd7b110d7474a725bdc3021d6e3313
Alex Tsai <al3x.tsai@gmail.com>
2015-12-30 00:55:36 +0800

    depth

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	for(var x = 0; x < 5; x++)
 	{
-		var cube = $("<a-cube color='#92A5A5' position='" + (x*3-6) + " " +Math.random()*3+ " -5'></a-cube>");
+		var cube = $("<a-cube color='#92A5A5' position='" + (x*3-6) + " " +(Math.random()*5-2)+ " " + (Math.random()*2 - 2)+ "'></a-cube>");
 		$('#scene').append(cube);
 		cube.append("<a-animation attribute='rotation' to='0 " + Math.random()*360 + " " + Math.random()*360 + "' dur='5000' direction='alternate' fill='forwards' repeat='indefinite' ease='linear'></a-animation>");
 		
